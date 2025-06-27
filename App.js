b58aec018ba1449c0496e8187e52d08d5c076b5d
matheus candido arrabal <matheuscandidoarrabal@gmail.com>
2025-06-26 22:51:44 -0300

    vamos

--- App.js
+++ App.js
@@ -1,30 +1,32 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from "./src/components/LoginScreen";
-import HomeScreen from "./src/components/HomeScreen";
-import ProfileScreen from "./src/components/ProfileScreen";
-import DetailsScreen from "./src/components/DetailsScreen";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
+import DetailsScreen from './screens/DetailsScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import 'react-native-reanimated';
 
-const Stack = createStackNavigator();
-    
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
-                <Stack.Screen
-                    name="Login"
-                    component={LoginScreen}
-                    options={{ headerShown: false }}
-                />
-                <Stack.Screen
-                    name="Home"
-                    component={HomeScreen}
-                    options={{ headerShown: false }} // Remove o botão de voltar
-                />
-                <Stack.Screen name="Profile" component={ProfileScreen} />
-                <Stack.Screen name="Details" component={DetailsScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }} // Esconde o cabeçalho no login
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerBackVisible: false }} // Oculta botão "voltar" na Home
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
